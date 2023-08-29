f56760dc6195153cfcc22e45f4dfef1ac869e190
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-08-29 17:59:42 -0400

    feat: base game render

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,71 @@
 <script lang="ts">
 	import { Canvas, Layer, type Render } from 'svelte-canvas';
 
+	type Node = number;
+	type Connection = [a: Node, b: Node];
+	type Vector = [x: number, y: number];
+	
+	const normalizeConnection = (connection: Connection) => connection.sort((a, b) => a - b);
+	let nodeCount = 6;
+	$: {
+		game.nodeCount = nodeCount;
+		game = game;
+	}
+
     interface Game {
-        
+		nodeCount: number;
+		connections: Connection[];
     }
+
+	let game: Game = {
+		nodeCount: 6,
+		connections: []
+	};
+
+	const padding = 100;
+
+	function angleToCoords(angle: number, width: number, height: number): Vector {
+		return [
+			(Math.sin(angle) + 1) * 1/2 * (width - padding) + padding / 2, 
+			(Math.cos(angle) + 1) * 1/2 * (height - padding) + padding / 2
+		]
+	}
 
     let render: Render;
 	$: render = ({ context, width, height }) => {
-		context.fillStyle = `hsl(${10 / 40}, 100%, 50%)`;
-		context.beginPath();
-		context.arc((10 / 4) % width, (1 / 4) % height, 100, 0, Math.PI * 2);
-		context.fill();
+		context.strokeStyle = "gray";
+		for (let i = 0; i < game.nodeCount; i++) {
+			const angle = i * (2 * Math.PI / game.nodeCount);
+			const [x, y] = angleToCoords(angle, width, height);
+			for (let j = 0; j < game.nodeCount; j++) {
+				const jAngle = j * (2 * Math.PI / game.nodeCount);
+				context.beginPath();
+				context.moveTo(x, y);
+				const [jx, jy] = angleToCoords(jAngle, width, height);
+				context.lineTo(jx, jy);
+				context.stroke();
+			}
+		}
+
+		context.strokeStyle = "black";
+		for (let i = 0; i < game.nodeCount; i++) {
+			const angle = i * (2 * Math.PI / game.nodeCount);
+			const [x, y] = angleToCoords(angle, width, height);
+			context.beginPath()
+			context.arc(
+				x,
+				y,
+				10,
+				0,
+				2 * Math.PI
+			)
+			context.fill()
+		}
 	};
 </script>
 
 <Canvas width={640} height={640}>
 	<Layer {render} />
 </Canvas>
+
+<input type="number" bind:value={nodeCount} />
